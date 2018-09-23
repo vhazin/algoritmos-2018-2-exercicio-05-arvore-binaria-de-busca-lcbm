a86152ec6104af979ca1beeb4701e2162a3dc6b5
Leonardo Castelo Branco de Melo <lcbm@cesar.school>
2018-09-23 20:22:24 -0300

    comitted first accepted version

--- lcbm.c
+++ lcbm.c
@@ -2,58 +2,81 @@
 #include <stdlib.h>
 
 typedef struct node{
-    int leaf;
-    struct node* leftBranch;
-    struct node* rightBranch;
+    int value;
+    struct node* leftChild, *rightChild;
 } Node;
 
-Node *plantTree(int leaf);
-void insert(Node *root, int leaf);
-void preOrder(Node *tree);
-void inOrder(Node *tree);
-void postOrder(Node *tree);
+Node *initNode(int);
+void insert(Node **, Node *);
+void preOrder(Node *);
+void inOrder(Node *);
+void postOrder(Node *);
 
 int main(void){
     short testCases;
     scanf("%hi", &testCases);
-    while(testCases--){
+    for (int test = 1; test <= testCases; test++){
         short nodes;
         scanf("%hi", &nodes);
-
-        Node *root = plantTree();
+        // Insert
+        Node *root = NULL;
         while(nodes--){
-            int leaf;
-            scanf("%d ", &leaf);
-            insert(root, leaf);
-
+            int value;
+            scanf("%d ", &value);
+            insert(&root ,initNode(value));
         }
-
-    printf("\n");
+        // Print results
+        printf("Case %d:\n", test);
+        printf("Pre.:");
+        preOrder(root);
+        printf("\nIn..:");
+        inOrder(root);
+        printf("\nPost:");
+        postOrder(root);
+        printf("\n\n");
+        
+        free(root);
     }
     return 0;
 }
 
-Node *plantTree(int leaf){
-    Node *root = (Node *)malloc(sizeof(Node));
-    root->leaf = leaf;
-    root->leftBranch = NULL;
-    root->rightBranch = NULL;
-
-    return root;
+Node *initNode(int value){
+    Node *child = malloc(sizeof(Node));
+    child->value = value;
+    child->leftChild = NULL;
+    child->rightChild = NULL;
+    return child;
 }
 
-void insert(Node *root, int leaf){
-
+void insert(Node **node, Node *child){
+    if(!*node)
+        *node = child;
+    else if (child->value <= (*node)->value)
+        insert(&(*node)->leftChild, child);
+    else
+        insert(&(*node)->rightChild, child);
 }
 
 void preOrder(Node *tree){
-
+    if (tree != NULL){
+        printf(" %d", tree->value);
+        preOrder(tree->leftChild);
+        preOrder(tree->rightChild);
+    }
 }
 
 void inOrder(Node *tree){
-
+    if (tree != NULL){
+        inOrder(tree->leftChild);
+        printf(" %d", tree->value);
+        inOrder(tree->rightChild);
+    }
 }
 
 void postOrder(Node *tree){
-
+    if (tree != NULL){
+        postOrder(tree->leftChild);
+        postOrder(tree->rightChild);
+        printf(" %d", tree->value);
+    }
 }
